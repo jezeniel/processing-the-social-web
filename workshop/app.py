eed4c757ed58d51b30cd9215ea0dd756f6de4abc
Jezeniel Zapanta <jezeniel.zapanta@lenddo.com>
2018-02-25 03:56:33 +0800

    Refactor

--- workshop/app.py
+++ workshop/app.py
@@ -4,9 +4,8 @@ import requests
 
 from facebook import GraphAPI, get_user_from_cookie
 from flask import Flask, jsonify, render_template, request
-from werkzeug.utils import secure_filename
 
-from .helpers import make_celery
+from .helpers import find_closest, make_celery, save_file
 from .models import Verification, db
 
 
@@ -27,45 +26,6 @@ app.config.update(
 
 celery = make_celery(app)
 db.init_app(app)
-
-
-def save_file(file):
-    filename = secure_filename(file.filename)
-    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(path)
-    return path
-
-
-def levenshtein(s1, s2):
-    if len(s1) < len(s2):
-        return levenshtein(s2, s1)
-
-    # len(s1) >= len(s2)
-    if len(s2) == 0:
-        return len(s1)
-
-    previous_row = range(len(s2) + 1)
-    for i, c1 in enumerate(s1):
-        current_row = [i + 1]
-        for j, c2 in enumerate(s2):
-            insertions = previous_row[j + 1] + 1
-            deletions = current_row[j] + 1
-            substitutions = previous_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-        previous_row = current_row
-
-    return previous_row[-1]
-
-
-def find_closest(text, words):
-    closest_distance = None
-    closest_word = None
-    for word in words:
-        distance = levenshtein(text.lower(), word.lower())
-        if closest_distance is None or closest_distance > distance:
-            closest_distance = distance
-            closest_word = word
-    return {'word': closest_word, 'distance': closest_distance}
 
 
 @celery.task()
--- workshop/helpers.py
+++ workshop/helpers.py
@@ -1,4 +1,7 @@
+import os
+
 from celery import Celery
+from werkzeug.utils import secure_filename
 
 
 def make_celery(flask_app):
@@ -19,3 +22,44 @@ def make_celery(flask_app):
 
     celery.Task = ContextTask
     return celery
+
+
+def save_file(file, path):
+    filename = secure_filename(file.filename)
+    path = os.path.join(path, filename)
+    file.save(path)
+    return path
+
+
+def levenshtein(s1, s2):
+    ''' find the edit distance between two strings '''
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
+def find_closest(text, words):
+    ''' find the closest distance between the `text` and a list of words '''
+    closest_distance = None
+    closest_word = None
+    for word in words:
+        distance = levenshtein(text.lower(), word.lower())
+        if closest_distance is None or closest_distance > distance:
+            closest_distance = distance
+            closest_word = word
+    return {'word': closest_word, 'distance': closest_distance}
